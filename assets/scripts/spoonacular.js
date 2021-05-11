2b69e70ffc1d936aa39a44703251a58f84778ae1
Tom Rumore <rumoretom@gmail.com>
2021-05-11 18:46:34 -0400

    created fetch with successful console log of data return

--- assets/scripts/spoonacular.js
+++ assets/scripts/spoonacular.js
@@ -1,6 +1,6 @@
 var apiKey = "0b52128a7a6341f58a4008807c5a9bca";
 
-fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=1")
+fetch("https://api.spoonacular.com/recipes/findByIngredients?apiKey=0b52128a7a6341f58a4008807c5a9bca&ingredients=apples,+flour,+sugar&number=1")
 .then(function(response) {
     return response.json();
 })
@@ -8,5 +8,5 @@ fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,
     console.log(data);
 })
 
-// sample api call formatting from the site. *this formatting works when using the apiKey var above.
+// sample api call formatting from the site. *this formatting works when using the above api
 //https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true.
