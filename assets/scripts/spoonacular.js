9228f76e5fc1da14d489f5e746f518eacd79a047
Tom Rumore <rumoretom@gmail.com>
2021-05-11 18:49:46 -0400

    successful console log of API fetch in spoonacularJS

--- assets/scripts/spoonacular.js
+++ assets/scripts/spoonacular.js
@@ -1,6 +1,6 @@
 var apiKey = "0b52128a7a6341f58a4008807c5a9bca";
 
-fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=1")
+fetch("https://api.spoonacular.com/recipes/findByIngredients?apiKey=0b52128a7a6341f58a4008807c5a9bca&ingredients=apples,+flour,+sugar&number=1")
 .then(function(response) {
     return response.json();
 })
