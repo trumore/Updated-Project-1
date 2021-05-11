51a191b7a80866761c6e2b7e88b5af0878c2f65e
Tom Rumore <rumoretom@gmail.com>
2021-05-11 17:36:16 -0400

    added fetch call, and example API formatting which i have proven out

--- assets/scripts/spoonacular.js
+++ assets/scripts/spoonacular.js
@@ -1,1 +1,12 @@
 var apiKey = "0b52128a7a6341f58a4008807c5a9bca";
+
+fetch("https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=1")
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+})
+
+// sample api call formatting from the site. *this formatting works when using the apiKey var above.
+//https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true.
