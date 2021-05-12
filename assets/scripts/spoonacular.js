629b99a5d9ba3bdf7e16619fd27e305406efe2d6
Tom Rumore <rumoretom@gmail.com>
2021-05-11 20:48:15 -0400

    created event listener to include url search

--- assets/scripts/spoonacular.js
+++ assets/scripts/spoonacular.js
@@ -1,12 +1,15 @@
 var apiKey = "0b52128a7a6341f58a4008807c5a9bca";
-var ingredientSearch = document.getElementById("submit-button")
+var ingredientButton = document.getElementById("submit-button");
 
 //Input button on click
 //  var ingredient= "ingredient".value();
 
 function getIngredient() {
-    var requestUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=0b52128a7a6341f58a4008807c5a9bca&ingredients=apples,+flour,+sugar&number=1"
+    event.preventDefault();
+    var ingredientSearch = document.getElementById("search-input").value;
+    var requestUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=0b52128a7a6341f58a4008807c5a9bca&ingredients=" + ingredientSearch + "&number=1"
     console.log("click");
+    console.log(ingredientSearch);
     fetch(requestUrl)
     .then(function(response) {
         return response.json();
@@ -19,7 +22,7 @@ function getIngredient() {
 };
 
 
-ingredientSearch.addEventListener("click", getIngredient);
+ingredientButton.addEventListener("click", getIngredient);
 // sample api call formatting from the site. *this formatting works when using the apiKey var above.
 //https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true.
 //"https://api.spoonacular.com/recipes/findByIngredients?apiKey=0b52128a7a6341f58a4008807c5a9bca&ingredients=apples,+flour,+sugar&number=1"
